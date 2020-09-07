85e79fec3d366a3d48495419f3085d0d9f70c0cd
carefree-request-1153 <sgold5757@gmail.com>
2020-09-07 03:55:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var recipes = {eggs: 3}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  new = Object.assign({}, object, {[key]: value})
-  return new
+  return Object.assign({}, object, {[key]: value})
 }
