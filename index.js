634e4910a57edd1ca8906dd3ff37eb86dd3ae604
carefree-request-1153 <sgold5757@gmail.com>
2020-09-07 03:59:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-
+  var clone = Object.assign({}, object)
+  delete clone[key]
+  return clone
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
